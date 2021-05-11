fbb588bd87ee9cbdf07c3b9f3497d0d1eae777e2
Calin Puscas <calinpuscas@gmail.com>
2021-05-11 16:55:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// Carousel
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
